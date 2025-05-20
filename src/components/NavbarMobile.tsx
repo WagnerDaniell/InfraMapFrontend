b0049cc4623ac75cc416104c0045a8dd82f6a866
WagnerDaniell <wagnerdaniell06@gmail.com>
2025-05-19 22:00:26 -0300

    fix-repair in component NavBarMobile

--- src/components/NavbarMobile.tsx
+++ src/components/NavbarMobile.tsx
@@ -2,11 +2,12 @@ import "../styles/NavbarMobileStyle.css"
 import "../styles/Global.css"
 import mais from "../assets/mais.png"
 import perfil from "../assets/perfil.png"
+import {Link} from "react-router-dom"
 
 const NavbarMobile = () => {
     return(
         <div className='navbar-mobile'>
-            <button className="bt-navbar-mobile"><img src={mais} alt="mais" width={25} />Criar Point</button>
+            <Link to="/criarpoint"><button className="bt-navbar-mobile"><img src={mais} alt="mais" width={25} />Criar Point</button></Link>
             <button className="bt-navbar-mobile"><img src={perfil} alt="perfil" width={25} />Perfil</button>
         </div>
     )
