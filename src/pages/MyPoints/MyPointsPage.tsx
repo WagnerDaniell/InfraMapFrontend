2a23a5b65a50fddc1337709ae404625f1f048977
WagnerDaniell <wagnerdaniell06@gmail.com>
2025-05-29 18:23:34 -0300

    fix-bugs

--- src/pages/MyPoints/MyPointsPage.tsx
+++ src/pages/MyPoints/MyPointsPage.tsx
@@ -19,103 +19,106 @@ interface MyToken {
 }
 
 interface MyPoint {
-    _id: string;
-    name: string;
-    description: string;
+  _id: string;
+  name: string;
+  description: string;
 }
 
 const MyPointsPage = () => {
-    const navigate = useNavigate()
-    const [meusPoints, setMeusPoints] = useState<MyPoint[]>([])
-    const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate()
+  const [meusPoints, setMeusPoints] = useState<MyPoint[]>([])
+  const [isLoading, setIsLoading] = useState(false)
 
-    useEffect(() => {
-        const buscarMyPoints = async () => {
-            try{
-                setIsLoading(true)
-                const token = localStorage.getItem("token")
-                if(token){
-                    const decodetoken = jwtDecode<MyToken>(token)
-                    const response = await axios.get(`https://inframap-back-end-3zs0.onrender.com/points/${decodetoken.userId}`)
-                    console.log(response)
-                    setMeusPoints(response.data)
-                    setIsLoading(false)
-                };
-            }catch(error){
-                setIsLoading(false)
-                navigate(-1)
-            }
-        };
-
-        buscarMyPoints();
-    },[])
-
-    const deletarpoint = async(id: string) => {
-        setIsLoading(true)
-        try{
-            if(id){
-                const token = localStorage.getItem("token")
-                await axios.delete(`https://inframap-back-end-3zs0.onrender.com/points/delete/${id}`, {
-                headers: {
-                    'Authorization' : `Bearer ${token}`,
-                }
-            })
-                toast.info("Point Deletado com sucesso!", {icon: <MdError color="#1F3B4D" size={26} />});
-            };
-        }catch(error){
-            setIsLoading(false)
-            if (axios.isAxiosError(error)){
-                const erroMsg = error.response?.data.error
-                toast.error(erroMsg, {icon: <MdError color="#1F3B4D" size={24} />});
-            }else{
-                toast.error("Erro ao atualizar.", {icon: <MdError color="#1F3B4D" size={26} />});
-            }
+  useEffect(() => {
+    const buscarMyPoints = async () => {
+      setIsLoading(true)
+      try {
+        const token = localStorage.getItem("token")
+        if (token) {
+          const decodetoken = jwtDecode<MyToken>(token)
+          const response = await axios.get(`https://inframap-back-end-3zs0.onrender.com/points/${decodetoken.userId}`)
+          setMeusPoints(response.data)
         }
-    };  
+      } catch (error) {
+        console.error(error)
+      } finally {
+        setIsLoading(false)
+      }
+    }
 
+    buscarMyPoints()
+  }, [])
 
-    return(
-        <div>
-            {isLoading && <Loading/>}
+  const deletarpoint = async (id: string) => {
+    setIsLoading(true)
+    try {
+      const token = localStorage.getItem("token")
+      await axios.delete(`https://inframap-back-end-3zs0.onrender.com/points/delete/${id}`, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        }
+      })
+      toast.info("Point Deletado com sucesso!", { icon: <MdError color="#1F3B4D" size={26} /> })
+      setMeusPoints(prev => prev.filter(point => point._id !== id))
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        const erroMsg = error.response?.data.error
+        toast.error(erroMsg, { icon: <MdError color="#1F3B4D" size={24} /> })
+      } else {
+        toast.error("Erro ao atualizar.", { icon: <MdError color="#1F3B4D" size={26} /> })
+      }
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
-            <Navbar showLandingPage = {false} showBack = {true}/>
+  return (
+    <div>
+      {isLoading && <Loading />}
 
-            <div className="container-mypoints">
-                <div className="titlemypoints-wrapper">
-                    <img src={lista} alt="lista" width={30}/>
-                    <p>Minha Lista de Points</p>
-                </div>
+      <Navbar showLandingPage={false} showBack={true} />
 
-                {meusPoints.length === 0 ? (
-                    <div className="sem-points">Não há nenhum point criado por você.</div>
-                ) : (
-                    meusPoints.map((point) => (
-                    <div key={point._id} className="mypoints-wrapper">
-                        <div className="mypoints-wrapper-left">
-                        <p>Nome do Point: {point.name}</p>
-                        <p>Descrição: {point.description}</p>
-                        </div>
-                        <div
-                        className="mypoints-wrapper-right"
-                        onClick={() => navigate('/editarpoint', { state: { id: point._id } })}
-                        >
-                        <button className="btn-wrapper-right" onClick={() => {navigate("/editarpoint", {state : {id: point._id}})}}>
-                            <img src={editar} alt="editar" width={30} />
-                        </button>
-
-                        <button className="btn-wrapper-right" onClick={() => {deletarpoint(point._id)}}>
-                            <img src={lixeira} alt="lixeira" width={30} />
-                        </button>
-                        </div>
-                    </div>
-                    ))
-                )}
-                
-            </div>
-
-            <Footer/>
+      <div className="container-mypoints">
+        <div className="titlemypoints-wrapper">
+          <img src={lista} alt="lista" width={30} />
+          <p>Minha Lista de Points</p>
         </div>
-    )
+
+        {meusPoints.length === 0 ? (
+          <div className="sem-points">Não há nenhum point criado por você.</div>
+        ) : (
+          meusPoints.map((point) => (
+            <div key={point._id} className="mypoints-wrapper">
+              <div className="mypoints-wrapper-left">
+                <p>Nome do Point: {point.name}</p>
+                <p>Descrição: {point.description}</p>
+              </div>
+              <div
+                className="mypoints-wrapper-right"
+                onClick={() => navigate('/editarpoint', { state: { id: point._id } })}
+              >
+                <button className="btn-wrapper-right" onClick={(e) => {
+                  e.stopPropagation()
+                  navigate("/editarpoint", { state: { id: point._id } })
+                }}>
+                  <img src={editar} alt="editar" width={30} />
+                </button>
+
+                <button className="btn-wrapper-right" onClick={(e) => {
+                  e.stopPropagation()
+                  deletarpoint(point._id)
+                }}>
+                  <img src={lixeira} alt="lixeira" width={30} />
+                </button>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+
+      <Footer />
+    </div>
+  )
 }
 
 export default MyPointsPage
